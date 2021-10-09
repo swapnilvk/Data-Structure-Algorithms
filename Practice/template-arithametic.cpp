217aba4c0d6d1d8ff4d6b829fab72dd7197dab4b
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2021-10-09 23:41:06 +0530

    template arithametic program

---
+++ Practice/template-arithametic.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+template <class T>
+class Arithametic {
+	T a;
+	T b;
+public:
+	void get(const T & x, const T & y);
+	T add();
+	T sub();
+};
+
+template <class T>
+void Arithametic <T> :: get(const T & x, const T & y){
+	a=x;
+	b=y;
+}
+
+template <class T>
+T Arithametic <T> :: add() {
+	T z = a+b;
+	return z;
+}
+
+template <class T>
+T Arithametic <T> :: sub() {
+	T z = a-b;
+	return z;
+}
+
+int main() {
+	Arithametic<int> obj;
+	obj.get(10, 20);
+	cout<<obj.add()<<endl;
+	cout<<obj.sub()<<endl;
+
+	Arithametic<float> objf;
+	objf.get(3.14, 3.14);
+	
+	cout<<objf.add()<<endl;
+	cout<<objf.sub()<<endl;
+}
